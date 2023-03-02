c17d519d69aa161d781a933b81a50489bf89d0f7
Brian <b.perez3237@gmail.com>
2023-03-02 16:37:16 -0500

    Write views and models

--- server/api/views.py
+++ server/api/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
+import rest_framework.status
+from .models import Account, Social
+from .serializers import AccountSerializer, SocialSerializer
 
 # Create your views here.
 
+def ShowAccount(request):
+    # qu
+    serializer_class = AccountSerializer
+
+def IndexAccount(request):
+    accounts = Account.objects.values()
+    print(list(accounts))
+    return JsonResponse(list(accounts), status=status.HTTP_200_OK, safe=False)
